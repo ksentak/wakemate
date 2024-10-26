e7a4c134a8b30ab8051fc551c5d22e15ed335334
Keaton Sentak <ksentak01@gmail.com>
2024-10-25 21:21:35 -0400

    revert implementation since wasnt working

--- src/components/ToggleSwitch.vue
+++ src/components/ToggleSwitch.vue
@@ -32,9 +32,9 @@ const isPreventingSleep = ref(false);
 
 const toggle = async () => {
   if (isPreventingSleep.value) {
-    await invoke('allow_sleep_command');
+    await invoke('allow_sleep');
   } else {
-    await invoke('prevent_sleep_command');
+    await invoke('prevent_sleep');
   }
   // Toggle the state after invoking the correct function
   isPreventingSleep.value = !isPreventingSleep.value;
